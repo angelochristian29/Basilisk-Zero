48b4cf4a701f68707fc6b048708fd04ca122092c
saimsheikh1 <saim.sheikh123@gmail.com>
2023-06-14 22:51:31 -0700

    Keycard to elevator access
    Blocking access to elevator without completing mission to find keycard

--- Basilisk-Zero/Assets/Scripts/AreaExit.cs
+++ Basilisk-Zero/Assets/Scripts/AreaExit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class AreaExit : MonoBehaviour
 {
     [SerializeField] private GameObject visualCue;
@@ -16,25 +17,50 @@ public class AreaExit : MonoBehaviour
     void Start()
     {
         areaEnter.transitionAreaName = transitionAreaName;
+        // gameObject.SetActive(false);
+       
     }
 
+
+// When you activate game object
     private void Awake() {
         playerInRange = false;
         visualCue.SetActive(false);
+
+       
+       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+      //Check item list for Level 2 Keycard, then allow player to enter elevator
+
+         
+
         if (playerInRange) {
             visualCue.SetActive(true);
-            if (Input.GetButtonUp("Fire1")) {
+            if (Input.GetButtonUp("Fire1") && (Inventory.instance.isInInventory("Level_2_Keycard"))) {
                 PlayerController.instance.transitionName = transitionAreaName;
                 SceneManager.LoadScene(sceneToLoad);
             }
         } else {
             visualCue.SetActive(false);
         }
+
+        // if (Inventory.instance.isInInventory("Level_2_Keycard")) {
+        //       //player can enter elevator
+        //         Debug.Log("player can enter elevator");
+        //         visualCue.SetActive(true);
+        //         if (Input.GetButtonUp("Fire1")) {
+        //             PlayerController.instance.transitionName = transitionAreaName;
+        //             SceneManager.LoadScene(sceneToLoad);
+        //         }
+        //         else{
+        //             visualCue.SetActive(false);
+        //         }
+        //     }
     }
 
     private void OnTriggerEnter2D(Collider2D collider) {
@@ -48,4 +74,6 @@ public class AreaExit : MonoBehaviour
             playerInRange = false;
         }
     }
+
+     
 }
