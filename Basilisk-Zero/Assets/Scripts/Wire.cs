e8d21ed69f7cd67f8909e70f60dd82d6b6da1e96
Christian Hernandez <71484821+angelochristian29@users.noreply.github.com>
2023-09-24 19:25:38 -0700

    Revert "Merge new main to saim"

---
+++ Basilisk-Zero/Assets/Scripts/Wire.cs
@@ -0,0 +1,13 @@
+// using UnityEngine'
+
+// public class Wire: MonoBehaviour{
+//     private Image _image;
+//     private void Awake(){
+//         _image= GetComponent<Image>();
+
+//     }
+//     public void SetColor(Color color){
+//         _image.color = color;
+//     }
+
+// }
