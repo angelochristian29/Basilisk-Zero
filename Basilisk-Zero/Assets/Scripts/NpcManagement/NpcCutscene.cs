c5ad0fef20b1521f839cfcf000952e3d3acda505
razzacktiger <harrazzack@gmail.com>
2023-11-14 23:25:59 -0800

    changed 28th floor guard

--- Basilisk-Zero/Assets/Scripts/NpcManagement/NpcCutscene.cs
+++ Basilisk-Zero/Assets/Scripts/NpcManagement/NpcCutscene.cs
@@ -15,7 +15,7 @@ public class NpcCutscene : MonoBehaviour
     public bool isMoving; // Flag indicating if the NPC is currently moving
     protected Vector3 walkDirection3D; // The direction to walk towards
 
-    public bool NPCTrigger1 = true; // Flag indicating if the dialogue should be triggered
+    public bool NPCTrigger1; // Flag indicating if the dialogue should be triggered
     public bool NicoDialogueDone; // Flag indicating if the dialogue is done
     // public PlayerController playerController; // The PlayerController component
 
@@ -38,8 +38,9 @@ public class NpcCutscene : MonoBehaviour
             return; // Exit the function
         }
         // if the player is in range then trigger then set the NPCTrigger1 flag to true
-        if (Vector2.Distance(PlayerController.instance.playerRB.transform.position, rb.transform.position) < 4.0f && NPCTrigger1 == true)
+        if (Vector2.Distance(PlayerController.instance.playerRB.transform.position, rb.transform.position) < 4.0f)
         {
+            NPCTrigger1 = true;
             // Check if Nico object is authorized to move and if the dialogue needs to be triggered
             TriggerNPC(NPCTrigger1, PlayerController.instance.playerRB);
         }
@@ -72,10 +73,9 @@ public class NpcCutscene : MonoBehaviour
             return; // Exit the function
         }
         
-        else if (Vector2.Distance(moveToObject.transform.position, rb.transform.position) < 4.0f)
+        else if (Vector2.Distance(moveToObject.transform.position, rb.transform.position) < 2.0f)
         {
             isMoving = false; // Set the isMoving flag to false
-            NPCTrigger1 = true;
             InkDialogueManager.GetInstance().EnterDialogueMode(inkJSON); // Start the ink dialogue
             Debug.Log("Dialogue started");
             Debug.Log("Dialogue started");
